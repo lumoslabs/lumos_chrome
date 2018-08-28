eddc903786abde9365271eb6083834a4fa22385a
Ryan <rmease@lumoslabs.com>
2018-08-28 15:30:36 -0700

    Update to remove new CSP format

--- js/background.js
+++ js/background.js
@@ -1,20 +1,20 @@
 // hijack web requests made by chrome to remove security headers
 chrome.webRequest.onHeadersReceived.addListener(
-    function(info) {
-        var headers = info.responseHeaders;
-        for (var i=headers.length-1; i>=0; --i) {
-            var header = headers[i].name.toLowerCase();
-            if (header == 'x-frame-options' || header == 'frame-options') {
-                headers.splice(i, 1); // Remove header
-            }
-        }
-        return {responseHeaders: headers};
-    },
-    {
-        urls: [ 'https://*.lumosity.com/*' ], 
-        types: [ 'sub_frame' ]
-    },
-    ['blocking', 'responseHeaders']
+  function (info) {
+    var headers = info.responseHeaders;
+    for (var i = headers.length - 1; i >= 0; --i) {
+      var header = headers[i].name.toLowerCase();
+      if (header == 'x-frame-options' || header == 'frame-options' || header == 'content-security-policy') {
+        headers.splice(i, 1); // Remove header
+      }
+    }
+    return { responseHeaders: headers };
+  },
+  {
+    urls: ['https://*.lumosity.com/*'],
+    types: ['sub_frame']
+  },
+  ['blocking', 'responseHeaders']
 );
 
 games = [
@@ -132,7 +132,7 @@ games2 = [
     slug: 'speed-match-overdrive',
     name: 'Speed Match Overdrive'
   },
-    {
+  {
     slug: 'splitting-seeds',
     name: 'Splitting Seeds'
   },
